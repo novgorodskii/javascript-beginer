69aa69f9ae4da5f1eef9a9ae1c8dc841c89d0bf1
novgorodskii <novgorodskiji@gmail.com>
2018-03-01 17:12:00 +0200

    var

--- day 1/js/plugin.js
+++ day 1/js/plugin.js
@@ -3,10 +3,21 @@
 // #1.1 Привет, мир!
 // #1.2 Внешние скрипты, порядок исполнения
 
-alert('Я - JavaScript!');
+// alert('Я - JavaScript!');
 
 // #1.3 Структура кода
 
 // #1.4 Современный стандарт, "use strict"
 
+// #1.5 Переменные
 
+var admin;
+var name;
+
+name = 'Василий';
+admin = name;
+
+// alert(admin);
+
+var planet = "Земля";
+var UserName = "Петя";
