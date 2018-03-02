f1220f593990333bead15c47d02f43406dfe095a
novgorodskii <novgorodskiji@gmail.com>
2018-03-02 15:25:22 +0200

    second day

--- day 1/js/plugin.js
+++ day 1/js/plugin.js
@@ -11,13 +11,56 @@
 
 // #1.5 Переменные
 
-var admin;
-var name;
+// var admin;
+// var name;
 
-name = 'Василий';
-admin = name;
+// name = 'Василий';
+// admin = name;
 
 // alert(admin);
 
-var planet = "Земля";
-var UserName = "Петя";
+// var planet = "Земля";
+// var UserName = "Петя";
+
+// Шесть типов данных, typeof
+
+// alert(1 / 0); //  NaN, ошибка
+// 
+// var string = "Мама мыла раму";
+// string = 'Одиночные кавички тоже пойдут'
+
+// typeof
+
+// typeof undefined // "undefined"
+// typeof 0 // "number"
+// typeof true // "boolean"
+// typeof 'foo' // "string"
+// typeof {} // "object" (1)
+// typeof function() // "fintion" (2)
+// {}
+
+// Основные операторы
+
+// var a = "моя" + "строка";
+// 
+// alert(a); 
+// моя строка
+
+// var a = 2;
+// var x = 1 + (a *= 2);
+
+// Операторы сравнения и логические значения
+
+// alert( undefined > 0); // false (1)
+// alert ( undefined < 0 ); // false (2)
+// alert ( undefined == 0); // false (3)
+
+// Побитовые операторы
+
+// Взаимодействие с пользователем: alert, prompt, confir
+
+var userName = prompt('User Name', 'Name');
+
+alert(userName);
+
+
