293588bb8db3db79ff40886e2a5652f59a6f9527
novgorodskii <novgorodskiji@gmail.com>
2018-03-03 17:35:21 +0200

    !, ||, &&

---
+++ day 2/js/plugin.js
@@ -0,0 +1,43 @@
+// Логические операторы
+
+// || (ИЛИ)
+
+// var result;
+// result = a || b;
+
+// alert(true || true); // true
+// alert(false || true); // true
+// alert(true || false); // true
+// alert(false || false) // false
+
+// if (1 || 0) { // сработает как if(true || false)
+//   alert("верно");
+// }
+
+// var hour = prompt('time', '');
+
+// if ( hour < 10 || hour > 18) {
+//   alert('офис до 10 или после 18 закрыт');
+// }
+
+// && (И)
+
+// alert(true && true) // true
+// alert(true && false) // false
+// alert(false && false) // false
+// alert(false && true) // false
+
+// var hour = 12;
+//   minute = 30;
+
+// if (hour == 12 && minute == 30) {
+//   alert( 'Время 12:30');
+// }
+
+// !(НЕ)
+
+// var age = 22;
+
+// if(!( age >= 14 && age <= 90));
+
+// if( age > 14 || age > 90);
